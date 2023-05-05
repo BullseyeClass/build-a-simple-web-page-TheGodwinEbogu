92d8f3f058911183abd091f54cddb078c8392e7e
TheGodwinEbogu <godwinebogu@gmail.com>
2023-05-06 00:42:50 +0100

    second commit

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-document.getElementById("result").innerHTML += 'Button clicked';
+// document.getElementById("result").innerHTML = "Button clicked";
+const result = document.querySelector("#result");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", function () {
+  alert("HELLO WORLD!");
+  result.style.display = "block";
+});
